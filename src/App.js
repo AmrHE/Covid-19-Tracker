58fe398e792a0e9d2c94d4d12e1f5401525f2687
AmrHE <amr.hassan.emam@gmail.com>
2021-03-31 06:47:20 +0200

    Project modification

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import React, { useEffect, useState } from 'react';
-import { FormControl, Select, MenuItem } from '@material-ui/core';
+import { FormControl, Select, MenuItem, Card, CardContent } from '@material-ui/core';
+import InfoBox from './InfoBox';
+import Map from './Map';
+import Table from './Table';
+import { sortData } from './util';
+import LineGraph from './LineGraph';
 import './App.css';
 
 function App() {
 
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
+  //Fetch the worlwide Data
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+    .then(response => response.json())
+    .then(data => {
+      setCountryInfo(data);
+    })
+  }, [])
+
+
+  //Fetch the countries list
   useEffect(() => {
     const getCountriesData = async () => {
       await fetch("https://disease.sh/v3/covid-19/countries")
       .then((response) => response.json())
       .then((data) => {
-        const countries = data.map((country) => (
-          {
-            name: country.country,
-            value: country.countryInfo.iso2,
+        const countries = data.map((country) => ({
+          name: country.country,
+          value: country.countryInfo.iso2,
             
-          }));
+        }));
+
+
+        const sortedData = sortData(data);
+
+        setTableData(sortedData);
         setCountries(countries);
       });
     };
@@ -25,33 +47,65 @@ function App() {
   }, []);
 
 
+  const onCountryChange = async (e) => {
+    const countryCode = e.target.value;
+    
+    const url = countryCode === 'worldwide' ? 
+    'https://disease.sh/v3/covid-19/all' : 
+    `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    
+    await fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      setCountry(countryCode);
+      //All the data we got of a single country from the API response
+      setCountryInfo(data);
+    });
+  };
+
+  console.log(countryInfo);
 
   return (
     <div className="app">
-      {/* Header */}
-      <div className="app__header">
-      {/* Title + Select Input dropdown field */}
-        <h1>Covid-19 Tracker</h1>
-        <FormControl className="app__dropdown">
-          <Select variant="outlined" value={country}>
-          <MenuItem value="worldwide" >Worldwide</MenuItem>
-          {countries.map(country => (
-              <MenuItem key={country.name} Value={country.value}>{country.name}</MenuItem>
-          ))}
-          </Select>
-        </FormControl>
+      <div className="app__left">
+        <div className="app__header">
+          <h1>Covid-19 Tracker</h1>
+          <FormControl className="app__dropdown">
+            <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide" >Worldwide</MenuItem>
+            {countries.map(country => (
+              <MenuItem key={country.name} value={country.value}>{country.name}</MenuItem>
+            ))}
+            </Select>
+          </FormControl>
+        </div>
+        <div className="app__stats">
+          <InfoBox 
+            title="Coronavirus Cases" 
+            cases={countryInfo.todayCases} 
+            total={countryInfo.cases} 
+          />
+          <InfoBox 
+            title="Recovered" 
+            cases={countryInfo.todayRecovered} 
+            total={countryInfo.recovered} 
+          />
+          <InfoBox 
+            title="Deaths" 
+            cases={countryInfo.todayDeaths} 
+            total={countryInfo.deaths} 
+          />
+        </div>
+        <Map />
       </div>
-      
-      
-      {/* Cases Boxes */}
-      {/* Recovered Boxes */}
-      {/* Deaths Boxes */}
-
-      {/* Table */}
-      {/* Graph */}
-
-      {/* Map */}
-
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          <Table countries={tableData} />
+          <h3>Worldwide new cases</h3>
+          <LineGraph />
+        </CardContent>
+      </Card>
     </div>
   );
 }
