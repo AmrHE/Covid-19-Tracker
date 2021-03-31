fb1d5e7d80415b925d0527af90cba366270b8715
AmrHE <amr.hassan.emam@gmail.com>
2021-03-31 06:39:31 +0200

    Add InfoBox.js

---
+++ src/InfoBox.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Card, CardContent, Typography } from "@material-ui/core";
+
+function InfoBox({title, cases, total}) {
+  return (
+    <div>
+      <Card className="infoBox">
+        <CardContent>
+          {/*Title*/}
+          <Typography className="infoBox__title" color="textSecondary">{title}</Typography>
+
+          {/*Cases*/}
+          <h2 className="infoBox__Cases">{cases}</h2>
+
+          {/*Total*/}
+          <Typography className="infoBox__total" color="textSecondary">{total}</Typography>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+export default InfoBox
