c898504b7903d2e751a7547bc0f86e14f8148eec
AmrHE <amr.hassan.emam@gmail.com>
2021-04-03 22:50:05 +0200

    build Map component

--- src/Map.js
+++ src/Map.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from 'react';
+import "./Map.css";
+import { MapContainer, useMap, TileLayer } from "react-leaflet";
+import { showDataOnMap } from './util';
 
-function Map() {
+function Map({ casesType, countries, center, zoom }) {
+
+  function ChangeView({ center, zoom }) {
+    const map = useMap();
+    map.setView(center, zoom);
+    return null;
+  }
+
   return (
-    <div className="map">
-      <h1>I am a Map</h1>
-    </div>
+    <MapContainer 
+      className="map"
+      center={center}
+      zoom={zoom}
+    >
+      <ChangeView center={center} zoom={zoom} />
+        <TileLayer 
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' 
+        />
+      {showDataOnMap(countries, casesType)}
+    </MapContainer>
   )
 }
 
