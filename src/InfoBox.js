604b4cec24694fc64df1ac1540157200db9be945
AmrHE <amr.hassan.emam@gmail.com>
2021-04-05 19:03:25 +0200

    add props

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -2,22 +2,19 @@ import React from 'react';
 import { Card, CardContent, Typography } from "@material-ui/core";
 import "./InfoBox.css";
 
-function InfoBox({title, cases, total}) {
+function InfoBox({title, cases, isRed, active, total, ...props }) {
   return (
-    <div>
-      <Card className="infoBox">
+      <Card onClick={props.onClick} className={`infoBox ${active && 'infoBox--selected'} ${isRed && 'infoBox--red'}`}>
         <CardContent>
-          {/*Title*/}
-          <Typography className="infoBox__title" color="textSecondary">{title}</Typography>
-
-          {/*Cases*/}
-          <h2 className="infoBox__Cases">{cases}</h2>
-
-          {/*Total*/}
-          <Typography className="infoBox__total" color="textSecondary">{total}</Typography>
+          <Typography className="infoBox__title" color="textSecondary">
+            {title}
+          </Typography>
+          <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>{cases}</h2>
+          <Typography className="infoBox__total" color="textSecondary">
+            {total} Total
+          </Typography>
         </CardContent>
       </Card>
-    </div>
   )
 }
 
