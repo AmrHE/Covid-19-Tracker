135888656bccc2a1974dc54f92b482e4efb79213
AmrHE <amr.hassan.emam@gmail.com>
2021-04-05 23:06:16 +0200

    reorganizing

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { Card, CardContent, Typography } from "@material-ui/core";
 import "./InfoBox.css";
 
-function InfoBox({title, cases, isRed, active, total, ...props }) {
+function InfoBox({title, cases, total, active, isRed, ...props }) {
   return (
-      <Card onClick={props.onClick} className={`infoBox ${active && 'infoBox--selected'} ${isRed && 'infoBox--red'}`}>
+      <Card 
+        onClick={props.onClick} 
+        className={
+          `infoBox 
+          ${active && 'infoBox--selected'} 
+          ${isRed && 'infoBox--red'}`
+        }
+        >
+          
         <CardContent>
           <Typography className="infoBox__title" color="textSecondary">
             {title}
