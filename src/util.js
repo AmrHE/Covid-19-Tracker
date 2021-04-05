125542e82cdf1f333bc01203777ea9980f4af10a
AmrHE <amr.hassan.emam@gmail.com>
2021-04-05 23:14:11 +0200

    bug fixing

--- src/util.js
+++ src/util.js
@@ -6,24 +6,27 @@ const casesTypeColors = {
   cases: {
     hex: "#CC1034",
     rgb: "rgb(204, 16, 52)",
+    half_op: "rgba(204, 16, 52, 0.5)",
     multiplier: 200,
   },
   recovered: {
     hex: "#7dd71d",
     rgb: "rgb(125, 215, 29)",
+    half_op: "rgba(125, 215, 29, 0.5)",
     multiplier: 300,
   },
   deaths: {
     hex: "#fb4443",
     rgb: "rgb(251, 68, 67)",
+    half_op: "rgba(251, 68, 67, 0.5)",
     multiplier: 500,
   },
-}
+};
 
 
 //Sort the country table Data with cases
 export const sortData = (data) => {
-  const sortedData = [...data];
+  let sortedData = [...data];
   sortedData.sort((a, b) => {
     if(a.cases > b.cases) {
       return -1;
@@ -37,17 +40,21 @@ export const sortData = (data) => {
 
 //Reformat the InfoBox numbers
 export const prettyPrintStat = (stat) =>
- stat ? `+${numeral(stat).format("0.0a")}` : "+0";
+stat ? `+${numeral(stat).format("0.0a")}` : "+0";
 
 //Draw circles on the map with interactive tooltips
-export const showDataOnMap = (data, casesType = "cases") => (
-  data.map(country => (
+export const showDataOnMap = (data, casesType) => (
+  data.map((country) => (
     <Circle
       center={[country.countryInfo.lat, country.countryInfo.long]}
       fillOpacity={0.4}
-      color={casesTypeColors[casesType].hex}
-      fillColor={casesTypeColors[casesType].hex}
-      radius={Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier}
+      pathOptions={{
+        color: casesTypeColors[casesType].hex, 
+        fillColor: casesTypeColors[casesType].hex 
+      }}      
+      radius={
+        Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+      }
     >
       <Popup>
         <div className="info-container">
